ce72f12963f4b14f0cc0c3e58e204b058a536426
whateverfornowthen <natashaalker212@gmail.com>
2025-08-10 16:27:31 +0100

    Add custom exception for task not found

---
+++ src/main/java/uk/gov/hmcts/reform/dev/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.dev.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleTaskNotFound(TaskNotFoundException ex) {
+        return Map.of("message", ex.getMessage());
+    }
+}
